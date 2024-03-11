d38a11a577e6c814f9d5a71be997e80cb8899222
Koen Cuijpers <kghcuijpers@gmail.com>
2024-03-11 14:14:27 -0500

    Update ruff settings using new notation

--- capptain/settings/dev.py
+++ capptain/settings/dev.py
@@ -3,5 +3,5 @@ from decouple import config as env
 from .base import *
 
 SECRET_KEY = env("SECRET_KEY")
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "172.18.0.2"]  # noqa: S104
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "172.18.0.2"]
 DEBUG = True
