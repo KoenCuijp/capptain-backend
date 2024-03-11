01620d31cc97dd5364c4e67c3b682ed2e7c837fc
Koen Cuijpers <kghcuijpers@gmail.com>
2024-03-11 16:45:32 -0500

    Comply to default project structure

---
+++ settings/base.py
@@ -0,0 +1,75 @@
+"""
+Contains the settings for the Capptain project.
+
+decouple.config searches for the secrets in the following order:
+    1. Take the secret from an evironment value if it's set there
+    2. Take the secret from the .env file (or .ini if your using that)
+    3. Take the secret from the default value (2nd parameter of config())
+
+This way we keep secrets out of our codebase and we have the option of overriding
+a secret with an environment variable (no strict need to deploy to override a secret).
+"""
+
+import os
+from pathlib import Path
+
+# Provides easy access to the base path of the project
+# It takes the parent.parent.parent directory of this file
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Tell django which urls.py to use first (the one at project root)
+ROOT_URLCONF = "urls"
+
+INSTALLED_APPS = [
+    "capptain",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
