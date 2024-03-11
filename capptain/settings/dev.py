eb9b8d9ce1098f2f11346aafdad2676170979e79
Koen Cuijpers <kghcuijpers@gmail.com>
2024-03-11 14:16:46 -0500

    Make dev key a static fake secret

--- capptain/settings/dev.py
+++ capptain/settings/dev.py
@@ -2,6 +2,6 @@ from decouple import config as env
 
 from .base import *
 
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = "fake-secret-key-for-dev-environment"
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "172.18.0.2"]
 DEBUG = True
