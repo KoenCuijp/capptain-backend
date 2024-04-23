3b70939c1efcf459861459c7cd755fa5a4b2c2af
Koen Cuijpers <koencuijp@Koens-MacBook-Air.local>
2024-04-23 13:03:56 +0200

    Add migrations & Model for TeamPlayer

--- capptain/models.py
+++ capptain/models.py
@@ -1,7 +1,30 @@
+from enum import StrEnum
+
+from django.contrib.auth.models import User
 from django.db import models
 
 
-# Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=40)
+    address = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class PlayerRole(StrEnum):
+    CAPTAIN = "Captain"
+    PLAYER = "Player"
+
+
+class TeamPlayers(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    player = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.CharField(
+        max_length=20,
+        choices=[(role.name, role.value) for role in PlayerRole],
+    )
+
+
 class Match(models.Model):
     opponent = models.CharField(max_length=50)
     home_away = models.CharField(
